1cda9b9ddb09cb94055ecdb955eec69a0df16ed6
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-08-01 09:39:07 -0400

    able to render views from index.HBS

--- index.js
+++ index.js
@@ -5,26 +5,35 @@ app.set("view engine", "hbs")
 const bodyParser = require("body-parser")
 const parser = app.use(bodyParser.urlencoded({ extended: true }))
 
+//render views on webpage
+
+app.get("/", (req,res) => {
+    res.render("index")
+})
+app.get('/profile/:id', (req, res) => {
+    User.findOne({ _id: req.params.id})
+    .then(user => {
+        res.render('users/show', {user})
+    })
+})
 
 //routes below here
 app.use(require('./routes/login.js'))
 app.use(require('./routes/signup.js'))
 app.use(require('./routes/workout.js'))
 
-app.get("/:name", (req,res) => {
-    res.render(`hello + ${req.params.name}`)
-})
+
 
 app.get("/:id", (req, res) => {
 
 })
   
-app.post('/', (req, res) => {
-    Workout.create(req.body)
-      .then(myNewItem => {
-        res.redirect('/')
-      })
-  })
+// app.post('/', (req, res) => {
+//     Workout.create(req.body)
+//       .then(myNewItem => {
+//         res.redirect('/')
+//       })
+//   })
 
 
 app.listen(4000, () => console.log("running on port 4000!!!"))
