6ea836ad1ed000baf0160d60d3470d24f1a64ff4
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-08-03 11:54:56 -0400

    heroku commit and node setup

--- db/connection.js
+++ db/connection.js
@@ -5,3 +5,9 @@ mongoose.connect("mongodb://localhost/workout")
 mongoose.Promise = Promise
 
 module.exports = mongoose
+
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect("mongodb://localhost/whenpresident");
+  }
